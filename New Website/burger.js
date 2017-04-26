a457697f77dfd290b19e1c6943b585091f7108ff
hiomcor <coreyhiom@gmail.com>
2017-04-26 22:50:20 +0100

    New website

---
+++ New Website/burger.js
@@ -0,0 +1,15 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    var Nav = document.getElementById("mySidenav")
+    if (Nav.style.width != "25vw") {
+        document.getElementById("mySidenav").style.width = "25vw";
+    }
+    else {
+        document.getElementById("mySidenav").style.width = "0";
+    }
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
